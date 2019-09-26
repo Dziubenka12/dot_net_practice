e5926d1293be07cb3325fa15130b82ed9681d6d1
Nastyaromanovsk <Nastyaromanovskaya55@gmail.com>
2019-09-26 21:22:17 +0300

    TimeOfDay

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Now.TimeOfDay;
+            if (today.Hours > 9 && today.Hours < 12)
+            {
+                Console.WriteLine($"Good morning, guys {today}");
+            }
+            else if (today.Hours > 12 && today.Hours < 15)
+            {
+                Console.WriteLine($"Good day, guys {today}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
