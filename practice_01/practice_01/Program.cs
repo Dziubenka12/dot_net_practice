4ac186adcf1759153379efaaee0049352e6ffb07
Dziubenko Anatoli <49936231+Dziubenka12@users.noreply.github.com>
2019-09-27 08:31:36 +0300

    TimeofDay

---
+++ practice_01/practice_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace practice_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var today = DateTime.Now.TimeOfDay;
+            if (today.Hours >= 9 && today.Hours < 12) 
+            {
+                Console.WriteLine($"Good Morning, guys {today}");
+            }
+            else if (today.Hours >= 12 && today.Hours < 15)
+            {
+                Console.WriteLine($"Good day, guys {today}");
+            }
+            else if (today.Hours >= 15 && today.Hours < 22)
+            {
+                Console.WriteLine($"Good evening, guys {today}");
+            }
+            else 
+            {
+                Console.WriteLine($"Good night, guys {today}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
