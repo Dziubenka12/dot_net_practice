56258316a00380064450356cd48cf46008ca1286
Nastyaromanovsk <Nastyaromanovskaya55@gmail.com>
2019-10-03 20:43:03 +0300

    task_3

--- lecture6/lecture6/Program.cs
+++ lecture6/lecture6/Program.cs
@@ -7,10 +7,24 @@ namespace lecture6
         static void Main(string[] args)
         {
             EmptyArray();
+            MaxNumber();
         }
         static void EmptyArray()
         {
             string[] array = new string[] { };
+        }
+        static void MaxNumber()
+        {
+            int max = 0;
+            int[] randArr = new int[13];
+            Random rand = new Random();
+            for (int i = 0; i < randArr.Length; i++)
+            {
+                randArr[i] = rand.Next(100);
+                Console.WriteLine(randArr[i]);
+                if(max < randArr[i]) { max = randArr[i]; }
+            }
+            Console.WriteLine($"Максимальное число {max}");
         }
     }
 }
