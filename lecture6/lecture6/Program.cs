6d74d4c23354adc17732a2dcf47e3e34116442bb
Nastyaromanovsk <Nastyaromanovskaya55@gmail.com>
2019-10-03 19:44:49 +0300

    task_1

---
+++ lecture6/lecture6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lecture6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmptyArray();
+        }
+        static void EmptyArray()
+        {
+            string[] array = new string[] { };
+        }
+    }
+}
